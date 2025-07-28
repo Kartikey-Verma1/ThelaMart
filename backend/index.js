69bff9de6bfc66d78b84444965a3c30d482c0247
Kartikey-Verma1 <kartik.v7391@gmail.com>
2025-07-28 13:53:06 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -93,4 +93,4 @@ app.get("/providers", async (req, res) => {
 
 
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("Server running on port", process.env.PORT || 5000));
